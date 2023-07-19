9d528bcfc44018b0e722b860a3c6ae1f00acf36c
yermakhan angsagan <ermahanansagan@gmail.com>
2023-07-20 00:03:52 +0600

    feat: benefits block

---
+++ src/pages/mainPage/components/BenefitCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps(["number", "title", "description"]);
+</script>
+
+<template>
+  <div class="card">
+    <div class="card__number">
+      {{ number }}
+    </div>
+    <h3 class="card__title">{{ title }}</h3>
+    <p class="card__description">{{ description }}</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 320px;
+  height: 100%;
+  border-radius: 32px;
+  background: #f5f5f7;
+  padding: 24px;
+  margin: 12px;
+}
+.card__number {
+  border: 1px solid #00e183;
+  border-radius: 50%;
+  color: #00e183;
+  font-size: 24px;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 48px;
+  width: 48px;
+}
+.card__title {
+  margin-top: 32px;
+}
+
+.card__description {
+  margin-top: 16px;
+}
+</style>
---
+++ src/pages/mainPage/widgets/Benefits.vue
@@ -0,0 +1,32 @@
+<script setup>
+import InfoBlock from "@/pages/mainPage/components/InfoBlock.vue";
+import BenefitCard from "@/pages/mainPage/components/BenefitCard.vue";
+
+const benefits = [
+  {
+    title: "Знания для преумножения капитала",
+    description:
+      "Знания — приносящие доход, лучшее описание курсов от Freedom Academy. Мы приоткроем занавес в мир трейдинга и покажем, как на этом можно приумножить своё благосостояние. После обучения вы сможете зарабатывать на внутридневных и среднесрочных сделках из любой точки мира, имея лишь доступ в интернет.",
+  },
+  { title: "Сертификат", description: "В последний день занятий мы организуем фотосессию с выпускниками и проведём вручение сертификатов." },
+  {
+    title: "Лекционный материал",
+    description:
+      "Вам будут доступны лекционные материалы по программе курса, а также домашние задания для проверки и закрепления пройденного материала. Вы получите персональные комментарии от наших экспертов по выполненным заданиям.",
+  },
+];
+</script>
+
+<template>
+  <InfoBlock :title="'Что вы получите'">
+    <div class="benefits">
+      <BenefitCard v-for="(benefit, index) in benefits" :key="index" :number="index + 1" :title="benefit.title" :description="benefit.description" />
+    </div>
+  </InfoBlock>
+</template>
+
+<style scoped lang="scss">
+.benefits {
+  display: flex;
+}
+</style>
