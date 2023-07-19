2defe61fd8addabea4c4eb4136a9c8e788987277
yermakhan angsagan <ermahanansagan@gmail.com>
2023-07-19 17:38:20 +0600

    feat: info panel

---
+++ src/pages/mainPage/components/InfoPanel.vue
@@ -0,0 +1,18 @@
+<script setup>
+import InfoPanelItems from '../components/InfoPanelItems.vue'
+
+const panelText = [{title: '11 марта 2021', description: 'Дата проведения'}, {title: '19:00 мск', description: 'Время проведения'}, {title: 'Алматы', description: 'Место проведения'}, {title: '5', description: 'Модулей'}]
+</script>
+
+<template>
+    <div class="info-panel">
+        <InfoPanelItems v-for="(item, index) in panelText" :key="index" :title="item.title" :description="item.description" />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.info-panel {
+    display: flex;
+    margin: 48px;
+}
+</style>
---
+++ src/pages/mainPage/components/InfoPanelItems.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(['title', 'description'])
+</script>
+
+<template>
+    <div class="info-panel__item">
+        <div class="info-panel__items-title">
+            {{ title }}
+        </div>
+        <div class="info-panel__items-description">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.info-panel__item {
+    text-align: center;
+    flex: 1;
+}
+.info-panel__items-title {
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+}
+</style>
