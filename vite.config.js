6d4cfce53a86a71620a62a1bb1c23c979522a642
yermakhan angsagan <ermahanansagan@gmail.com>
2023-07-19 19:57:46 +0600

    chore: adding alias

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue() ],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src ", import.meta.url)),
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
-  },  
-})
+  },
+});
