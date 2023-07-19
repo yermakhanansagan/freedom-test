bb99cbcddb98ae211443a3f7c9bd7fa52ba912a3
yermakhan angsagan <ermahanansagan@gmail.com>
2023-07-19 15:31:46 +0600

    feat: add config

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: ["plugin:vue/recommended", "plugin:import/recommended", "plugin:security/recommended", "@vue/eslint-config-prettier"],
+  };
+  
