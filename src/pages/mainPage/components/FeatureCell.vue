0a964b231f16079623585c1e030849d71bbaffcd
yermakhan angsagan <ermahanansagan@gmail.com>
2023-07-19 19:59:11 +0600

    feat: courses for block

---
+++ src/pages/mainPage/components/FeatureCell.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps(['imgName', 'title', 'description'])
+</script>
+
+<template>
+    <div class="feature-cell">
+        <img class="feature-cell__icon" :src="'/src/assets/' + imgName" :alt="imgName">
+        <div class="feature-cell__texts">
+            <h3 class="feature-cell__title">{{ title }}</h3>
+            <p class="feature-cell__description">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.feature-cell {
+    width: 100%;
+    display: flex;
+    align-items: start;
+}
+.feature-cell__icon {
+    width: 64px;
+    height: 64px;
+    background: #F5F5F7;
+    border-radius: 8px;
+}
+.feature-cell__texts {
+    margin-left: 24px;
+}
+.feature-cell__title {
+    padding: 0;
+    margin: 0;
+}
+</style>
